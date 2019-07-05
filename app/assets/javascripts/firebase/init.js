462251a5b0b9558dfed5cc851cdf7b326e7ab629
saheb <saheb@yottolabs.com>
2019-07-05 16:56:28 +0530

    removed information

--- app/assets/javascripts/firebase/init.js
+++ app/assets/javascripts/firebase/init.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBp7IyYiGwS9rtNdxQXt6lo-i9Tkyj6rJ0",
-    authDomain: "testingapp-saheb222.firebaseapp.com",
-    databaseURL: "https://testingapp-saheb222.firebaseio.com",
-    projectId: "testingapp-saheb222",
-    storageBucket: "testingapp-saheb222.appspot.com",
-    messagingSenderId: "805443709825",
-    appId: "1:805443709825:web:23edc2fa9156bb20"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
